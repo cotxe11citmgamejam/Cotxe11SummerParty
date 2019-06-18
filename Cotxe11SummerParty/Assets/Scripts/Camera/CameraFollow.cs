7362d87710b2dcf52afccee8645215ba4bbda789
manelmourelo <manelmourelo.1@gmail.com>
2019-06-18 15:13:33 +0200

    Camera

---
+++ Cotxe11SummerParty/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public float z_distance = 5;
+    public float y_distance = 10;
+    public GameObject target = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 new_pos = new Vector3();
+        new_pos.x = target.gameObject.transform.position.x;
+        new_pos.y = target.gameObject.transform.position.y + y_distance;
+        new_pos.z = target.gameObject.transform.position.z + z_distance;
+        transform.SetPositionAndRotation(new_pos, transform.rotation);
+    }
+}

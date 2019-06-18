e5f5b03473203a941c158c00a1526f854eb55e51
isaac calvis <isaaccalvis@gmail.com>
2019-06-18 11:03:30 +0200

    Added circuitPart

---
+++ Cotxe11SummerParty/Assets/Scripts/CircuitScripts/CircuitGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircuitGenerator : MonoBehaviour
+{
+
+    public GameObject circuitPart;
+    public Transform nextCircuitPoint;
+
+    private bool startDestroying = false;
+    private float destroyingTempo = 0.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (startDestroying)
+        {
+            destroyingTempo += Time.deltaTime;
+            if (destroyingTempo > 10)
+                Destroy(gameObject);
+        }
+    }
+
+	void OnTriggerEnter(Collider collision)
+	{
+        if (collision.gameObject.tag == "Player")
+        {
+            GenerateNewPart();
+        }
+	}
+
+    void GenerateNewPart()
+    {
+        GameObject nextOne = Instantiate(circuitPart, nextCircuitPoint.position , Quaternion.identity);
+        startDestroying = true;
+    }
+}

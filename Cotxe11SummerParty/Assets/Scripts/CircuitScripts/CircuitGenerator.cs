20f31eecda3bc043e33cb7ba7ba30f316d2ab362
isaac calvis <isaaccalvis@gmail.com>
2019-06-18 12:02:05 +0200

    throw items added

--- Cotxe11SummerParty/Assets/Scripts/CircuitScripts/CircuitGenerator.cs
+++ Cotxe11SummerParty/Assets/Scripts/CircuitScripts/CircuitGenerator.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class CircuitGenerator : MonoBehaviour
 {
-
+    // Internal
     public GameObject circuitPart;
     public GameObject spawnPoint;
     public Transform nextCircuitPoint;
 
+    // External
+    public GameObject emptyBox;
+    
+    // Internal Variables
     private bool startDestroying = false;
     private float destroyingTempo = 0.0f;
+    private float throwItemTempo = 0.0f;
+
+    public float timeToThrowAnItem = 5.0f;
+    public float timeToKillAnItem = 10.0f;
 
     void Start()
     {
-        float newRandPosToObjectSpawners = Random.RandomRange(-5, 5);
-        spawnPoint.GetComponent<Transform>().position = new Vector3 (newRandPosToObjectSpawners, spawnPoint.GetComponent<Transform>().position.y, spawnPoint.GetComponent<Transform>().position.z);
     }
 
     void Update()
@@ -26,20 +32,43 @@ public class CircuitGenerator : MonoBehaviour
             if (destroyingTempo > 10)
                 Destroy(gameObject);
         }
+
+        throwItemTempo += Time.deltaTime;
+        if (throwItemTempo > timeToThrowAnItem)
+        {
+            float newRandPosToObjectSpawners = Random.RandomRange(-5, 5);
+            spawnPoint.GetComponent<Transform>().position = new Vector3(newRandPosToObjectSpawners, spawnPoint.GetComponent<Transform>().position.y, spawnPoint.GetComponent<Transform>().position.z);
+            ThrowAnItem();
+            throwItemTempo = 0.0f;
+        }
     }
 
 	void OnTriggerEnter(Collider collision)
 	{
         if (collision.gameObject.CompareTag("Player"))
         {
-            print("chock");
             GenerateNewPart();
         }
 	}
 
     void GenerateNewPart()
     {
-        GameObject nextOne = Instantiate(circuitPart, nextCircuitPoint.position , Quaternion.identity);
+        GameObject nextOne = Instantiate(circuitPart, nextCircuitPoint.position , Quaternion.identity); // Genera una nova peça del circuit
         startDestroying = true;
+    }
+
+    void ThrowAnItem()
+    {
+        int itemNum = Random.RandomRange(0, 0); // Numero total d'items
+        switch (itemNum)
+        {
+            case 0:
+                GameObject itemThrown = Instantiate(emptyBox, spawnPoint.transform.position, Quaternion.identity);
+                Destroy(itemThrown, timeToKillAnItem);
+                break;
+            case 1:
+
+                break;
+        }
     }
 }
---
+++ Cotxe11SummerParty/Assets/Scripts/CircuitScripts/EmptyBox.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptyBox : MonoBehaviour
+{
+    public GameObject box_usable;
+    public GameObject box_broken;
+
+    void Start()
+    {
+        box_usable.SetActive(true);
+        box_broken.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        box_usable.SetActive(false);
+        box_broken.SetActive(true);
+    }
+}

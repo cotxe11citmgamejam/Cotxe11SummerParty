aa0ac76135ad2b71898d012740edde178ab430d6
Ricardogll <rikilonga@gmail.com>
2019-06-19 02:39:20 +0200

    Start menu almost done

---
+++ Cotxe11SummerParty/Assets/StartMenuCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenuCamera : MonoBehaviour
+{
+
+    public GameObject cotxe;
+
+    public float radius = 1.0f;
+    public float rotate_speed = 5.0f;
+    public float height = 10.1f;
+    private float angle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle += rotate_speed * Time.deltaTime;
+        var offset = new Vector3(Mathf.Sin(angle) * radius, height, Mathf.Cos(angle) * radius);
+        transform.position = cotxe.transform.position + offset;
+
+
+
+        this.transform.LookAt(cotxe.transform);
+    }
+}
